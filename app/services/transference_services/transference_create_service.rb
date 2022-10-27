ef4e36149bae104aa46f8c2017eb9231ab917616
jbfermor <jbfermor@hotmail.com>
2022-10-27 17:10:07 +0200

    locking account for concurrence an errors

--- app/services/transference_services/transference_create_service.rb
+++ app/services/transference_services/transference_create_service.rb
@@ -3,8 +3,20 @@ module TransferenceServices
   class TransferenceCreateService < TransferenceReviewService
   
     def call
-      Transference.create(amount: @amount, receiver: @receiver, sender: @sender, final_balance: receiver_balance, account_id: receiver_account.id)
-      Transference.create(amount: @amount, receiver: @receiver, sender: @sender, final_balance: sender_balance, account_id: sender_account.id)
+
+      receiver_account.with_lock do
+
+        sender_account.with_lock do
+          
+          @sender_transference = Transference.new(amount: @amount, receiver: @receiver, sender: @sender, final_balance: sender_balance, account_id: sender_account.id)
+          if @sender_transference.save
+            Transference.create(amount: @amount, receiver: @receiver, sender: @sender, final_balance: receiver_balance, account_id: receiver_account.id)
+          end
+
+        end
+
+      end
+
     end
   
   end

80e6a4a02051d7b911bd8a8296a876d845c11828
jbfermor <jbfermor@hotmail.com>
2022-10-31 14:26:54 +0100

    tresnference_services refactor

--- app/services/transference_services/transference_create_service.rb
+++ app/services/transference_services/transference_create_service.rb
@@ -1,24 +1,54 @@
 module TransferenceServices
 
-  class TransferenceCreateService < TransferenceReviewService
+  class TransferenceCreateService
+
+    attr_reader :amount, :sender, :receiver
+
+    def initialize(amount, receiver, sender)
+      @amount = amount
+      @receiver = receiver
+      @sender = sender
+    end
   
     def call
-
       receiver_account.with_lock do
-
         sender_account.with_lock do
-          
           @sender_transference = Transference.new(amount: @amount, receiver: @receiver, sender: @sender, final_balance: sender_balance, account_id: sender_account.id)
           if @sender_transference.save
             Transference.create(amount: @amount, receiver: @receiver, sender: @sender, final_balance: receiver_balance, account_id: receiver_account.id)
           end
-
         end
-
       end
+    end 
 
+    def sender_account
+      User.find(@sender).account
+    end
+    
+    def receiver_account
+      User.find(@receiver).account
+    end
+
+    def sender_balance
+      sender_account.balance - @amount
+    end
+
+    def receiver_balance
+      receiver_account.balance + @amount
+    end
+
+    def possible?
+      different_users? && balance_enough?
     end
   
-  end
+    def different_users?
+      @sender != @receiver
+    end
+  
+    def balance_enough?
+      sender_account.balance - @amount >= 0
+    end
 
+  end
+  
 end
--- spec/services/transference_services/transference_create_service_spec.rb
+++ spec/services/transference_services/transference_create_service_spec.rb
@@ -4,14 +4,57 @@ RSpec.describe TransferenceServices::TransferenceCreateService do
 
   let!(:sender) { create(:sender) }
   let!(:receiver) { create(:receiver) }
-  let!(:sender_account) { create(:sender_account, user: sender) }
-  let!(:receiver_account) { create(:receiver_account, user: receiver)}
+  let!(:sender_account) { create(:account, balance: 100, user: sender) }
+  let!(:receiver_account) { create(:account, balance: 100, user: receiver) }
 
   describe "call" do
 
     it "should create a transference" do
       transference = TransferenceServices::TransferenceCreateService.new( 10, receiver.id, sender.id)
       expect{ transference.call }.to change{ Transference.count }.by(2)
+    end
+
+  end
+
+  describe "actions" do
+
+    it "get sender Account" do
+      transference = TransferenceServices::TransferenceCreateService.new( 10, receiver.id, sender.id)
+      expect(transference.sender_account).to eq(sender.account)
+    end
+
+    it "get receiver Account" do
+      transference = TransferenceServices::TransferenceCreateService.new( 10, receiver.id, sender.id)
+      expect(transference.receiver_account).to eq(receiver.account)
+    end
+
+    it "review if sender and receiver are different" do
+      transference = TransferenceServices::TransferenceCreateService.new( 10, receiver.id, sender.id)
+      expect(transference.different_users?).to be(true)
+    end
+
+  end
+  
+  describe "with account operations" do
+
+    it "review if sender balance is higher or equal than amount" do  
+      transference = TransferenceServices::TransferenceCreateService.new( 10, receiver.id, sender.id)
+      expect(transference.balance_enough?).to be(true)
+    end
+
+    it "review if transference is possible" do 
+      transference = TransferenceServices::TransferenceCreateService.new( 10, receiver.id, sender.id)
+      expect(transference.possible?).to be(true)
+    end
+
+    it "get sender final balance" do
+      transference = TransferenceServices::TransferenceCreateService.new( 10, receiver.id, sender.id)
+      expect(transference.sender_balance).to eq(90)
+    end
+
+    it "get receiver final balance" do
+      transference = TransferenceServices::TransferenceCreateService.new( 10, receiver.id, sender.id)
+      expect(transference.receiver_balance).to eq(110)
     end
 
   end

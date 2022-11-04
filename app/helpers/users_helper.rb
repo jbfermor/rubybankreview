792f40471dcb5107e4a02fc95d488f36dc5abcf9
jbfermor <jbfermor@hotmail.com>
2022-11-04 08:44:47 +0100

    added views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,4 +10,16 @@ module UsersHelper
     end
   end
 
+    #Return CONSOLE if the transfer was through console
+  #Return sender username if was between users
+  def show_sender(sender)
+    sender == nil ? "CONSOLE" : User.find_by(account: Account.find(sender)).username
+  end
+
+  #Return THIS CCOUNT if the transfer was received
+  #Return receiver if was sent
+  def show_receiver(receiver)
+    Account.find(receiver) == current_user.account ? "This account" : User.find_by(account: Account.find(receiver)).username
+  end
+
 end
